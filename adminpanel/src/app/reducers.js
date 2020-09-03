436b4b843c1f41c89b8a8589ece3dd15c7c7bec8
shikhar-priyadarshi-vst-au4 <officialshikharid@gmail.com>
2020-09-03 09:04:01 +0530

    changes

--- adminpanel/src/app/reducers.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { loginSlice } from '../modules/Login/login.slice';
-import { panelSlice } from '../modules/Panel/panel.slice';
-
-export default combineReducers({
-    login: loginSlice.reducer,
-    panel: panelSlice.reducer
-})
--- adminpanel/src/modules/Login/login.services.js
+++
@@ -1,3 +0,0 @@
-import httpClient from '../../api/httpClient';
-
-export const logIn = (data) => httpClient('/users/sign_in', { method: "POST", data }, 'https://uat.alpha5.io/api/v1');
